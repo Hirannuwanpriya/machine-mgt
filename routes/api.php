208ad7b0711fb63e2555813d3313bf93ce35dc6f
Hiran Nuwanpriya <hirannuwanpriya@gmail.com>
2025-02-27 13:06:40 +0530

    Add User Authentication

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})
+->middleware('auth:sanctum');
+
+
+
+
