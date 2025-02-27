76886b9cc5f0ecdf1b72034f18ed9779e8bfc977
Hiran Nuwanpriya <hirannuwanpriya@gmail.com>
2025-02-27 17:11:25 +0530

    Update MachinesController

--- app/Http/Controllers/MachinesController.php
+++ app/Http/Controllers/MachinesController.php
@@ -24,7 +24,6 @@ class MachinesController extends Controller
     {
 
         return rescue(function() {
-
             $machines = $this->machineRepository->all($this->machineRepository->all(
                 paginate: true,
                 length: 2
@@ -53,8 +52,7 @@ class MachinesController extends Controller
 
     public function show($id): JsonResponse
     {
-        return rescue(function($id){
-
+        return rescue(function() use ($id) {
             $machine = $this->machineRepository->find($id);
 
             //get machine by id and return as json format
@@ -77,103 +75,86 @@ class MachinesController extends Controller
 
     public function store(Request $request): JsonResponse
     {
-        try {
-            $machine = $this->machineRepository->create($request->validate([
+
+        return rescue(function() use ($request) {
+           $validated = $request->validate([
                 'name' => 'required|string|max:255',
                 'purchase_date' => 'required|string|max:255',
                 'price' => 'required|string|max:255',
                 'category' => 'required|string|max:255',
                 'brand' => 'required|string|max:255',
-            ]));
+            ]);
 
-            //create user and return as  json format
+            $machine = $this->machineRepository->create($validated);
+
+            //return created machine as json format
             return response()->json([
-                'data' => $machine,
-                'meta' => [
-                    '_timestamp' => Carbon::now()->timestamp,
-                ],
-            ], 201);
-
-        } catch (\Exception $e) {
-
-            return response()->json([
-                'message' => $e->getMessage(),
-                'stack' => $e->getTraceAsString(),
-            ], 402);
-        }
-    }
-
-    public function update(Request $request, $id): JsonResponse
-    {
-        return $this->machineRepository->update($id, $request->all());
-
-        try {
-            $machine = $this->machineRepository->update($id, $request->validate([
-                'name' => 'required|string|max:255',
-                'purchase_date' => 'required|string|max:255',
-                'price' => 'required|string|max:255',
-                'category' => 'required|string|max:255',
-                'brand' => 'required|string|max:255',
-            ]));
-
-            //update user and return as  json format
-            return response()->json([
-                'data' => ['message' => $machine ? 'Machine updated' : 'Machine not found'],
-                'meta' => [
-                    '_timestamp' => Carbon::now()->timestamp,
-                ],
-            ], $machine ? 201 : 404);
-
-        } catch (\Exception $e) {
-
-            return response()->json([
-                'message' => $e->getMessage(),
-                'stack' => $e->getTraceAsString(),
-            ], 402);
-        }
-    }
-
-    public function destroy($id): JsonResponse
-    {
-        $machine = $this->machineRepository->delete($id);
-
-        try {
-            //delete user and return as  json format
-            return response()->json([
-                'data' => ['message' => $machine ? 'Machine deleted' : 'Machine not found'],
-                'meta' => [
-                    '_timestamp' => Carbon::now()->timestamp,
-                ],
-            ], $machine ? 200 : 404);
-
-        } catch (\Exception $e) {
-
-            return response()->json([
-                'message' => $e->getMessage(),
-                'stack' => $e->getTraceAsString(),
-            ], 402);
-        }
-    }
-
-    public function search(Request $request): JsonResponse
-    {
-        $search = $this->machineRepository->search($request->all());
-
-        try {
-            //delete user and return as  json format
-            return response()->json([
-                'data' => ['message' => $search ? $search : 'Machine not found'],
+                'status' => true,
+                'payload' => $machine,
                 'meta' => [
                     '_timestamp' => Carbon::now()->timestamp,
                 ],
             ], 200);
 
-        } catch (\Exception $e) {
+        }, function ($e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'stack' => $e->getTraceAsString(),
+            ], 402);
+        });
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+
+        return rescue(function() use ($request, $id) {
+            $validated = $request->validate([
+                'name' => 'required|string|max:255',
+                'purchase_date' => 'required|string|max:255',
+                'price' => 'required|string|max:255',
+                'category' => 'required|string|max:255',
+                'brand' => 'required|string|max:255',
+            ]);
+
+            $machine = $this->machineRepository->update($id, $validated);
+
+            //return updated machine as json format
+            return response()->json([
+                'status' => true,
+                'payload' =>  $machine ? 'Machine updated' : 'Machine not found',
+                'meta' => [
+                    '_timestamp' => Carbon::now()->timestamp,
+                ],
+            ], $machine ? 201 : 404);
+
+        }, function ($e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'stack' => $e->getTraceAsString(),
+            ], 402);
+        });
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        return rescue(function() use ($id){
+            $machine = $this->machineRepository->delete($id);
+
+            //get machine by id and return as json format
+            return response()->json([
+                'status' => true,
+                'payload' => $machine ? 'Machine deleted' : 'Machine not found',
+                'meta' => [
+                    '_timestamp' => Carbon::now()->timestamp,
+                ],
+            ], $machine ? 200 : 404);
+
+        }, function ($e) {
 
             return response()->json([
                 'message' => $e->getMessage(),
                 'stack' => $e->getTraceAsString(),
             ], 402);
-        }
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,32 @@
 <?php
 
+use App\Http\Controllers\MachinesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-
-
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })
 ->middleware('auth:sanctum');
+
+Route::get('/user/logout', [AuthController::class, 'logout'])
+    ->name('auth.logout');
+
+Route::prefix('machines')->controller(MachinesController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('machine.index');
+        Route::get('{id}', 'show')->name('machine.show');
+        Route::post('/', 'store')->name('machine.create');
+        Route::put('{id}', 'update')->name('machine.update');
+        Route::delete('{id}', 'destroy')->name('machine.delete');
+    })->middleware([
+        'auth:sanctum'
+    ]);
+
+
 
 
 
