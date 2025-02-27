f4d4b3112dcb24b23887155b7f78e93175e4da5f
Hiran Nuwanpriya <hirannuwanpriya@gmail.com>
2025-02-27 16:44:19 +0530

    Add Enums

---
+++ app/Enum/Brand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enum;
+
+enum Brand: int
+{
+    case Brand_1 = 1;
+    case Brand_2 = 2;
+    case Brand_3 = 3;
+    case Brand_4 = 4;
+}
---
+++ app/Enum/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enum;
+
+enum Category: int
+{
+    case Category_1 = 1;
+    case Category_2 = 2;
+    case Category_3 = 3;
+    case Category_4 = 4;
+}
