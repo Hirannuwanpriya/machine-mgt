285130ce56579c6ed636762e8451f1d45cce8e86
Hiran Nuwanpriya <hirannuwanpriya@gmail.com>
2025-03-05 01:00:50 +0530

    Update Machine to load modal

--- resources/js/pages/Machine.vue
+++ resources/js/pages/Machine.vue
@@ -10,8 +10,8 @@
           <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto"></div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-              <button @click="open = true"
-                  class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <button @click="openMachineForm(-1)"
+                      class="cursor-pointer block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Create Machine</button>
             </div>
           </div>
@@ -21,6 +21,7 @@
               <table class="min-w-full divide-y divide-gray-300">
                 <thead>
                 <tr>
+                  <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Name</th>
                   <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Name</th>
                   <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Date</th>
                   <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Category</th>
@@ -35,6 +36,7 @@
                 </thead>
                 <tbody class="divide-y divide-gray-200 bg-white">
                 <tr v-for="machine in machines.machines" :key="machine.id">
+                  <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">{{ machine.id }}</td>
                   <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">{{ machine.name }}</td>
                   <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">{{ useDateFormat(machine.purchase_date, 'DD/MM/YYYY') }}</td>
                   <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{ machine.category }}</td>
@@ -42,21 +44,21 @@
                   <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{ machine.hours }}</td>
                   <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{ machine.price }}</td>
                   <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-sm font-medium sm:pr-0 space-x-4">
-                    <RouterLink
-                        :to="{ name : 'machine.reset_timer' , params: { machineId : machine.id } }"
-                        class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-sky-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white">
-                      Reset<span class="sr-only">, {{ machine.id }}</span>
-                    </RouterLink>
-                    <RouterLink
-                        :to="{ name : 'machine.edit' , params: { machineId : machine.id } }"
+<!--                    <RouterLink-->
+<!--                        :to="{ name : 'machine.reset_timer' , params: { machineId : machine.id } }"-->
+<!--                        class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-sky-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white">-->
+<!--                      Reset<span class="sr-only">, {{ machine.id }}</span>-->
+<!--                    </RouterLink>-->
+                    <button
+                        @click.prevent="openMachineForm(machine.id)"
                         class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white">
                       Edit<span class="sr-only">, {{ machine.id }}</span>
-                    </RouterLink>
-                    <RouterLink
-                        :to="{ name : 'machine.delete' , params: { machineId : machine.id } }"
-                        class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-red-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white">
-                      Delete<span class="sr-only">, {{ machine.id }}</span>
-                    </RouterLink>
+                    </button>
+                    <button
+                        @click.prevent="openDeleteModal(machine.id)"
+                        class="cursor-pointer inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-red-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white">
+                      Delete
+                    </button>
                   </td>
                 </tr>
                 </tbody>
@@ -68,9 +70,14 @@
     </div>
   </main>
 
-  <template v-if="open">
-    <MachineForm @callParentMethod="toggleMachineForm"></MachineForm>
-  </template>
+  <teleport to="body">
+
+    <MachineForm v-if="showMachineForm"  :machineId="selectedMachineId"
+                 @close="showMachineForm = false"/>
+
+    <DeleteModal v-if="showDeleteModal" :machineId="selectedMachineId"
+                 @close="showDeleteModal = false"/>
+  </teleport>
 
 </template>
 
@@ -96,23 +103,25 @@ export default {
     }
   },
   components: {
-    DeleteModal,
-    MachineForm
-  },
-  computed: {
-
+    MachineForm,
+    DeleteModal
   },
   data() {
     return {
       open: false,
+      showMachineForm: false,
+      showDeleteModal: false,
+      selectedMachineId: null
     }
   },
-  mounted() {
-
-  },
   methods: {
-    toggleMachineForm() {
-      this.open = !this.open;
+    openMachineForm(machineId) {
+      this.selectedMachineId = machineId;
+      this.showMachineForm = true;
+    },
+    openDeleteModal(machineId) {
+      this.selectedMachineId = machineId;
+      this.showDeleteModal = true;
     }
   }
 }
