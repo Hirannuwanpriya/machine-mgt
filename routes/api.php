5bb638f5cfd2b966fa27ee4743d37e086b1aafba
Hiran Nuwanpriya <hirannuwanpriya@gmail.com>
2025-03-10 17:24:27 +0530

    remove duplicate route

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
 
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })
